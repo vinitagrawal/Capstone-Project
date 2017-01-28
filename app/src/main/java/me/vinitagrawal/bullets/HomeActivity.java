05c2a6c7923b27a4e7c14710bae9c980bfc4a652
Vinit Agrawal <vinitagrawal91@gmail.com>
2017-01-28 16:16:12 +0530

    Added Recycler view and menu option for sort order

--- app/src/main/java/me/vinitagrawal/bullets/HomeActivity.java
+++ app/src/main/java/me/vinitagrawal/bullets/HomeActivity.java
@@ -23,7 +23,7 @@ public class HomeActivity extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -42,20 +42,18 @@ public class HomeActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.sortRecent) {
+            return true;
+        } else if (id == R.id.sortTrending) {
             return true;
         }
 
